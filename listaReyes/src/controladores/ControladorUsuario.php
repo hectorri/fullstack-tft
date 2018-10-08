862692d4ad7724deb9b2882bf927e52f3433bacd
Héctor <hectorri@hotmail.es>
2018-10-08 18:11:55 +0200

    Corrección

--- listaReyes/src/controladores/ControladorUsuario.php
+++ listaReyes/src/controladores/ControladorUsuario.php
@@ -69,4 +69,49 @@ class ControladorUsuario {
 	}
 
 	/*-- Funciones del CRUD --*/
+
+	/**
+    * Función para crear un usuario
+    * @param type Slim\Http\Request $request - solicitud http
+    * @param type Slim\Http\Response $response - respuesta http
+    */
+    public function crea($request, $response, $args) {
+		/*
+		 getParsedBody() toma los parametros del cuerpo de $request que estén
+		 como json o xml y lo parsea de un modo que PHP lo  entienda 
+		*/
+		$param = $request->getParsedBody(); 
+        
+		$validaciones = $this->validaArgs($param); // hace las validaciones
+		if(verifica($validaciones)){
+		
+			// evalua si el correo ya existe en la base de datos
+            $correo_existente = Usuario::where('email', $atr['email'])->get()->first();
+        
+			// si el correo ya existe manda un error 403
+            if($correo_existente){
+                echo->$this->error('YA_ESTÁ_REGISTRADO_EL_CORREO',
+                                   $request->getUri()->getPath(),
+                                   404);
+                return $this->response->withStatus(403);
+            } else {
+            
+				//crea un nuevo usuario a partir del modelo
+                $usuario = new Usuario;
+
+                // asigna cada elemento del arreglo $atr con su columna en la tabla usuarios
+                $usuario->nombre = $atr['nombre'];
+				$usuario->apellidos = $atr['apellidos'];
+                $usuario->email = $atr['email'];
+                $usuario->contrasena = $atr['contrasena'];
+
+                $usuario->save(); //guarda el usuario
+
+                // crea una ruta para el usuario con su id
+                $path =  $request->getUri()->getPath() . '/' . $usuario->id;
+
+                return $response->withStatus(201); // el usuario fue creado con éxito
+            }
+		}
+	}
 }
